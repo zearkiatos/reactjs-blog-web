6816f66dd5cfc96fd4591a85396c794975dd95f8
zearkiatos <caprilespe@outlook.com>
2021-09-28 23:12:24 -0300

    Exploring the Posts Api

---
+++ src/config/index.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+const isDevelop = process.env.NODE_ENV === "develop";
+const config = {
+  ENVIRONMENT: process.env.NODE_ENV,
+  DEVELOP: isDevelop,
+  REDUXBLOG_API_BASE_URL: process.env.REACT_APP_REDUXBLOG_API_BASE_URL,
+  REDUXBLOG_API_KEY: process.env.REACT_APP_REDUXBLOG_API_KEY,
+};
+
+export default config;
