4657e1ec6288ecdf984cc4cbd4de4fbed33c5de4
zearkiatos <caprilespe@outlook.com>
2021-10-15 13:19:43 -0300

    Navigation with the Link Component

--- src/components/PostsIndex/index.js
+++ src/components/PostsIndex/index.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
+import { Link } from 'react-router-dom';
 import { postActions } from "../../actions";
 class PostsIndex extends Component {
   componentWillMount() {
     this.props.fetchPosts();
   }
   render() {
-    return <div>List of blog posts.</div>;
+    return (
+      <div>
+        <div className="ui one column grid">
+          <div className="row">
+            <div className="sixteen wide column">
+              <Link to="/posts/new" className="ui right floated primary button">
+                  Add a Post
+              </Link>
+            </div>
+          </div>
+        </div>
+        List of blog posts.
+      </div>
+    );
   }
 }
 
