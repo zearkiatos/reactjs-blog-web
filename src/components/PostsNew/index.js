bfb7e9a0810e7dcbf2d518bc88a6c86378deabec
zearkiatos <caprilespe@outlook.com>
2021-10-15 14:52:28 -0300

    More on Forms and Form Submission

--- src/components/PostsNew/index.js
+++ src/components/PostsNew/index.js
@@ -1,9 +1,34 @@
 import React, { Component } from "react";
+import { reduxForm } from "redux-form";
 
 class PostsNew extends Component {
   render() {
-    return <div>Create From</div>;
+    return (
+      <form className="ui form">
+        <h3>
+          Create a New Post
+        </h3>
+        <div className="field">
+          <label>Title</label>
+          <input type="text" />
+        </div>
+
+        <div className="field">
+          <label>Categories</label>
+          <input type="text"  />
+        </div>
+
+        <div className="field">
+          <label>Content</label>
+          <textarea></textarea>
+        </div>
+        <button type="submit" className="ui primary button">Submit</button>
+      </form>
+    );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: "PostsNewForm",
+  fields: ["title", "categories", "content"],
+})(PostsNew);
