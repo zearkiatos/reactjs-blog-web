047a767400eaa205ad9d3537db4098cd1d508cdc
zearkiatos <caprilespe@outlook.com>
2021-10-15 12:13:06 -0300

    Creating New Posts

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import PostsIndex from './components/PostsIndex';
+import PostsNew from './components/PostsNew';
 
 const Routes = () => (
   <Router history={createBrowserHistory}>
-    <Route path="/" component={PostsIndex} />
+    <Route path="/" component={PostsIndex} exact />
+    <Route path="/posts/new" component={PostsNew} exact />
   </Router>
 );
 
