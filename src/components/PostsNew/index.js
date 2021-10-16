457c26c89992e2fa51ccfda9b6141d504cb187d3
Pedro Capriles <caprilespe@outlook.com>
2021-10-16 13:44:48 -0300

    Form Validation Continued

--- src/components/PostsNew/index.js
+++ src/components/PostsNew/index.js
@@ -2,15 +2,26 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { Field, reduxForm } from "redux-form";
+import { Link } from 'react-router-dom';
 import { postActions } from '../../actions';
 
 class PostsNew extends Component {
   render() {
     const renderInput = ({ input, name, label, type, meta: { touched, error } }) => (
-      <div className="field">
+      <div className={`field  ${touched && error ? 'error' : ''}`}>
         <label>{label}</label>
         <div>
           <input {...input} placeholder={label} type={type} name={name} />
+          {touched && error && <div className="ui red message">{error}</div>}
+        </div>
+      </div>
+    );
+
+    const renderTextArea = ({ input, name, label, meta: { touched, error } }) => (
+      <div className={`field  ${touched && error ? 'error' : ''}`}>
+        <label>{label}</label>
+        <div>
+          <textarea {...input} placeholder={label} name={name}></textarea>
           {touched && error && <div className="ui red message">{error}</div>}
         </div>
       </div>
@@ -22,21 +33,17 @@ class PostsNew extends Component {
     return (
       <form className="ui form" onSubmit={() => handleSubmit(this.props.createPost)}>
         <h3>Create a New Post</h3>
-        <div className="field">
-          <Field name="title" label="Title" component={renderInput} type="text" />
-        </div>
+        <Field name="title" label="Title" component={renderInput} type="text" />
 
-        <div className="field">
-          <Field name="categories" label="Categories" component={renderInput} type="text" />
-        </div>
-
-        <div className="field">
-          <label>Content</label>
-          <Field name="content" component="textarea" />
-        </div>
+        <Field name="categories" label="Categories" component={renderInput} type="text" />
+        
+        <Field name="content" component={renderTextArea} label="Content" />
         <button type="submit" className="ui primary button" disabled={invalid}>
           Submit
         </button>
+        <Link to="/" className="ui negative button">
+          Cancel
+        </Link>
       </form>
     );
   }
@@ -51,6 +58,10 @@ function validate (values) {
 
   if(!values.categories) {
     errors.categories = 'Enter a categories';
+  }
+
+  if(!values.content) {
+    errors.content = 'Enter some content';
   }
 
   return errors;
