f02d70aeba5a97f828bfe78298827f8447a510bc
zearkiatos <caprilespe@outlook.com>
2021-10-17 14:16:09 -0300

    Dynamic Route Params

--- src/components/PostsIndex/index.js
+++ src/components/PostsIndex/index.js
@@ -1,34 +1,32 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { postActions } from "../../actions";
 class PostsIndex extends Component {
   componentWillMount() {
     this.props.fetchPosts();
   }
-  renderPosts = () => this.props.posts && this.props.posts.map((post) => (
-    <div className="item" key={post.id}>
-      <div className="content">
-      <div className="ui one column grid">
-          <div className="row">
-            <div className="sixteen wide column">
-              <span className="ui right floated">
-                {post.categories}
-              </span>
-              <div className="header">
-                {post.title}
+  renderPosts = () =>
+    this.props.posts &&
+    this.props.posts.map((post) => (
+      <div className="item" key={post.id}>
+        <div className="content">
+          <div className="ui one column grid">
+            <div className="row">
+              <div className="sixteen wide column">
+                <Link to={`posts/${post.id}`}>
+                  <span className="ui right floated">{post.categories}</span>
+                  <div className="header">{post.title}</div>
+                </Link>
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="description">
-
+          <div className="description"></div>
         </div>
       </div>
-    </div>
-  ));
+    ));
   render() {
     return (
       <div>
@@ -41,12 +39,8 @@ class PostsIndex extends Component {
             </div>
           </div>
         </div>
-        <h3>
-          Posts
-        </h3>
-        <div className="ui relaxed divided list">
-          {this.renderPosts()}
-        </div>
+        <h3>Posts</h3>
+        <div className="ui relaxed divided list">{this.renderPosts()}</div>
       </div>
     );
   }
@@ -54,8 +48,8 @@ class PostsIndex extends Component {
 
 function mapStateToProps(state) {
   return {
-    posts: state.posts.all
-  }
+    posts: state.posts.all,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import { Router, Route } from "react-router-dom";
-import PostsIndex from './components/PostsIndex';
-import PostsNew from './components/PostsNew';
-import history from './history';
+import PostsIndex from "./components/PostsIndex";
+import PostsNew from "./components/PostsNew";
+import PostsShow from "./components/PostsShow";
+import history from "./history";
 
 const Routes = () => (
   <Router history={history}>
     <Route path="/" component={PostsIndex} exact />
     <Route path="/posts/new" component={PostsNew} exact />
+    <Route path="/posts/:id" component={PostsShow} />
   </Router>
 );
 
