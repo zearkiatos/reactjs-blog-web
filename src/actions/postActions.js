834bea2a867fce2690d2b084bbf7fc2078fc1c1d
zearkiatos <caprilespe@outlook.com>
2021-10-18 13:42:41 -0300

    Delete Action Creator

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,36 +1,55 @@
-import axios from 'axios';
-import { POST_TYPES } from '../types';
-import config from '../config';
+import axios from "axios";
+import { POST_TYPES } from "../types";
+import config from "../config";
 
 const fetchPosts = () => {
-    const request = axios.get(`${config.REDUXBLOG_API_BASE_URL}/api/posts?key=${config.REDUXBLOG_API_KEY}`);
+  const request = axios.get(
+    `${config.REDUXBLOG_API_BASE_URL}/api/posts?key=${config.REDUXBLOG_API_KEY}`
+  );
 
-    return {
-        type: POST_TYPES.FETCH_POSTS,
-        payload: request
-    };
+  return {
+    type: POST_TYPES.FETCH_POSTS,
+    payload: request,
+  };
 };
 
 const createPost = (properties) => {
-    const request = axios.post(`${config.REDUXBLOG_API_BASE_URL}/api/posts?key=${config.REDUXBLOG_API_KEY}`, properties);
+  const request = axios.post(
+    `${config.REDUXBLOG_API_BASE_URL}/api/posts?key=${config.REDUXBLOG_API_KEY}`,
+    properties
+  );
 
-    return {
-        type: POST_TYPES.CREATE_POST,
-        payload: request
-    }
-}
+  return {
+    type: POST_TYPES.CREATE_POST,
+    payload: request,
+  };
+};
 
 const fetchPost = (id) => {
-    const request = axios.get(`${config.REDUXBLOG_API_BASE_URL}/api/posts/${id}?key=${config.REDUXBLOG_API_KEY}`);
+  const request = axios.get(
+    `${config.REDUXBLOG_API_BASE_URL}/api/posts/${id}?key=${config.REDUXBLOG_API_KEY}`
+  );
 
-    return {
-        type: POST_TYPES.FETCH_POST,
-        payload: request
-    }
-}
+  return {
+    type: POST_TYPES.FETCH_POST,
+    payload: request,
+  };
+};
+
+const deletePost = (id) => {
+  const request = axios.delete(
+    `${config.REDUXBLOG_API_BASE_URL}/api/posts/${id}?key=${config.REDUXBLOG_API_KEY}`
+  );
+
+  return {
+    type: POST_TYPES.DELETE_POST,
+    payload: request,
+  };
+};
 
 export default {
-    fetchPosts,
-    createPost,
-    fetchPost
-}
+  fetchPosts,
+  createPost,
+  fetchPost,
+  deletePost
+};
--- src/components/PostsShow/index.js
+++ src/components/PostsShow/index.js
@@ -2,16 +2,27 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { postActions } from "../../actions";
+import { Link } from "react-router-dom";
 
 class PostsShow extends Component {
   componentWillMount() {
     this.props.fetchPost(this.props.match.params.id);
   }
+  onDeleteClick = () => {
+    this.props.deletePost(this.props.match.params.id);
+  };
   render() {
     const { post } = this.props;
     if (post) {
       return (
         <div>
+          <Link to="/">Back to Index</Link>
+          <button
+            className="ui negative right floated button"
+            onClick={this.onDeleteClick.bind(this)}
+          >
+            Delete Post
+          </button>
           <h3>{post.title}</h3>
           <h6>Categories: {post.categories}</h6>
           <p>{post.content}</p>
@@ -26,6 +37,7 @@ const mapDispatchToProps = (dispatch) => {
   return bindActionCreators(
     {
       fetchPost: postActions.fetchPost,
+      deletePost: postActions.deletePost,
     },
     dispatch
   );
